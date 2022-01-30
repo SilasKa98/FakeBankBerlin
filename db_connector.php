9ef5f066d9503044cff16b510b9f19f1d3ba120f
StuartTheYellowOne <stuart.the.yellow.one@gmail.com>
2022-01-30 14:57:07 +0100

    files formatiert

--- db_connector.php
+++ db_connector.php
@@ -1,16 +1,14 @@
 <?php
 
 $connection = new mysqli("localhost:3307", "root");
-    if (mysqli_connect_errno()) {
-        printf("Connect failed: %s\n", mysqli_connect_error());
-        exit();
-    }
-
-    if (!$connection->select_db("fakeBankBerlin")) {
-        print "DB existiert nicht.";
-        $connection->close();
-        exit();
-    }
-	
+  if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+  }
+  if (!$connection->select_db("fakeBankBerlin")) {
+    print "DB existiert nicht.";
+    $connection->close();
+    exit();
+  }
 ?>
 
